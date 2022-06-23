48fdc0113a218a2ff753bdb01da86eade02a6f58
Rudi Ørnhøj <rornhoj@gmail.com>
2022-06-23 10:21:55 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<!-- prettier-ignore -->
+<template>
+  <div>
+    {{ myTes }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  data() {
+    const myTest = 'testing'
+
+    console.log(myTes)
+
+    return {
+      myTest,
+    }
+  },
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
